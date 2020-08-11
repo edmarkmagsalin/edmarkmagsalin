80ccc2966d780f335b819c8c16af5a9120bc7770
Edmark Magsalin <ejmagsalin@straightarrow.com>
2020-08-11 23:37:04 +0100

    Update: icons

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,20 +1,25 @@
 import React, { Component, Fragment } from 'react'
+import contactKeeper from '../images/thumbnails/contact-keeper.png'
+import githubFinder from '../images/thumbnails/github-finder.png'
+import MERNCRUD from '../images/thumbnails/mern-crud-bare-minimum.png'
 
 export class Projects extends Component {
+
     render() {
         return (
             <Fragment>
-                <a className="anchor-point" id="projects"></a>
+                <span className="anchor-point" id="projects">&nbsp;</span>
                 <section>
                     <div className="container">
                         <h2>Projects</h2>
-                        <div className="carousel">
+                        <div className="carousel mt-1">
+                            
                             <div className="carousel__item">
-                                <div className="two-column">
-                                    <div>
-                                        <div className="carousel__image" data-bg-image="contact-keeper.png"></div>
+                                <div className="row">
+                                    <div className="col-sm-12 col-lg-6">
+                                        <img className="carousel__image" src={contactKeeper} alt="Contact Keeper" />
                                     </div>
-                                    <div>
+                                    <div className="col-sm-12 col-lg-6">
                                         <h3>Contact Keeper</h3>
                                         <p className="lead">
                                             You can organize your personal and professional contacts using this application. This project is part of the course <strong>React Front To Back</strong> that is created by Brad Traversy.
@@ -25,15 +30,16 @@ export class Projects extends Component {
                                     </div>
                                 </div>
                             </div>
+
                             <div className="carousel__item">
-                                <div className="two-column">
-                                    <div>
-                                        <div className="carousel__image" data-bg-image="github-finder.png"></div>
+                                <div className="row">
+                                    <div className="col-sm-12 col-lg-6">
+                                        <img className="carousel__image" src={githubFinder} alt="GitHub User Finder" />
                                     </div>
-                                    <div>
-                                        <h3>Github Finder</h3>
+                                    <div className="col-sm-12 col-lg-6">
+                                        <h3>GitHub User Finder</h3>
                                         <p className="lead">
-                                            You can organize your personal and professional contacts using this application. This project is part of the course React Front To Back that is created by Brad Traversy.
+                                            You can organize your personal and professional contacts using this application. This project is part of the course <strong>React Front To Back</strong> that is created by Brad Traversy.
                                         </p>
                                         <p>
                                             Powered by: Node.js, Express, React, MongoDB
@@ -41,6 +47,25 @@ export class Projects extends Component {
                                     </div>
                                 </div>
                             </div>
+
+                            <div className="carousel__item">
+                                <div className="row">
+                                    <div className="col-sm-12 col-lg-6">
+                                        <img className="carousel__image" src={MERNCRUD} alt="MERN CRUD Bare Minimum" />
+                                    </div>
+                                    <div className="col-sm-12 col-lg-6">
+                                        <h3>MERN CRUD Bare Minimum</h3>
+                                        <p className="lead">
+                                            You can organize your personal and professional contacts using this application. This project is part of the course <strong>React Front To Back</strong> that is created by Brad Traversy.
+                                        </p>
+                                        <p>
+                                            Powered by: Node.js, Express, React, MongoDB
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+
                         </div>
                     </div>
                 </section>
--- server.js
+++ server.js
@@ -19,19 +19,13 @@ app.use((req, res, next) => {
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// An api endpoint that returns a short list of items
-// app.get('/api/list', (req,res) => {
-//     var list = ["item1", "item2", "item3"];
-//     res.json(list);
-// });
-
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App is listening to port ${PORT}`);
 });
